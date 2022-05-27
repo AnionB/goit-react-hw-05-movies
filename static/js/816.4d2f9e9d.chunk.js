853a42182f76479cc09d9689c8ac0e18cdcfa1d6
AnionB <90142322+AnionB@users.noreply.github.com>
2022-05-27 14:48:40 +0000

    Deploying to gh-pages from @ AnionB/goit-react-hw-05-movies@b50456682e6213940d8070c2cf68b004fdacdec2 🚀

---
+++ static/js/816.4d2f9e9d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[816],{2816:function(e,t,i){i.r(t);i(2791);var s=i(6871),c=i(184);t.default=function(){return(0,c.jsx)("div",{children:(0,c.jsx)(s.j3,{})})}}}]);
+//# sourceMappingURL=816.4d2f9e9d.chunk.js.map
